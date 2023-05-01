788bce3c547cc08ce50e7fecae3f5c756c9f05c0
Carmen Alvarez <carmen@rmen.ca>
2023-05-01 15:09:48 +0200

    Add a period at the end of the slack message.

--- withingsslack/services/slack.py
+++ withingsslack/services/slack.py
@@ -8,7 +8,7 @@ def post_weight(weight_data: WeightData):
     date_str = weight_data.date.strftime("%a %d %b %Y, %H:%M")
     message = (
         f"<@{weight_data.slack_alias}> weighed in at "
-        + f"{weight_data.weight_kg:.2f} kg on {date_str}"
+        + f"{weight_data.weight_kg:.2f} kg on {date_str}."
     )
     requests.post(
         url=settings.slack_webhook_url,
