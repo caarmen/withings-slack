7692501a0f529624fe5ca5eeff9ae6de63e4b7cf
Carmen Alvarez <carmen@rmen.ca>
2023-05-01 15:48:46 +0200

    Add timestamps to access logs

--- withingsslack/main.py
+++ withingsslack/main.py
@@ -16,6 +16,7 @@ from withingsslack.services.withings import oauth as withings_oauth
 database.init()
 
 log_format = "%(asctime)s [%(name)-14s] %(levelname)-8s %(message)s"
+LOGGING_CONFIG["formatters"]["access"]["fmt"] = log_format
 LOGGING_CONFIG["formatters"]["default"]["fmt"] = log_format
 logging.basicConfig(
     format=log_format,
