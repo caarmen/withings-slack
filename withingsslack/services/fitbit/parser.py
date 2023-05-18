8003b25980b325109a2b81e320e82010d0497cb9
Carmen Alvarez <carmen@rmen.ca>
2023-05-18 12:43:46 +0200

    Revert "Simplify support of classic sleep logs"
    This reverts commit 58509b922177d95350b20156da963fc0c989b62d.
    
    We need to use the `discriminator` to validate the json.
    
    Otherwise we'll incorrectly parse invalid data with `type`=`stages` and `summary` containing `asleep`.

--- withingsslack/services/fitbit/parser.py
+++ withingsslack/services/fitbit/parser.py
@@ -1,7 +1,7 @@
 import json
-from typing import Optional, Self
+from typing import Annotated, Literal, Optional, Self, Union
 from withingsslack.services import models as svc_models
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 import datetime
 
 
@@ -18,8 +18,12 @@ class FitbitStagesSleepItemSummary(BaseModel):
     wake: FitbitSleepItemSummaryItem
 
 
-class FitbitSleepItemLevels(BaseModel):
-    summary: FitbitClassicSleepItemSummary | FitbitStagesSleepItemSummary
+class FitbitStagesSleepItemLevels(BaseModel):
+    summary: FitbitStagesSleepItemSummary
+
+
+class FitbitClassicSleepItemLevels(BaseModel):
+    summary: FitbitClassicSleepItemSummary
 
 
 class FitbitSleepItem(BaseModel):
@@ -28,12 +32,25 @@ class FitbitSleepItem(BaseModel):
     endTime: str
     isMainSleep: bool
     startTime: str
-    levels: FitbitSleepItemLevels
-    type: str
+
+
+class FitbitClassicSleepItem(FitbitSleepItem):
+    type: Literal["classic"]
+    levels: FitbitClassicSleepItemLevels
+
+
+class FitbitStagesSleepItem(FitbitSleepItem):
+    type: Literal["stages"]
+    levels: FitbitStagesSleepItemLevels
 
 
 class FitbitSleep(BaseModel):
-    sleep: list[FitbitSleepItem]
+    sleep: list[
+        Annotated[
+            Union[FitbitClassicSleepItem, FitbitStagesSleepItem],
+            Field(discriminator="type"),
+        ]
+    ]
 
     @classmethod
     def parse(cls, input: str) -> Self:
