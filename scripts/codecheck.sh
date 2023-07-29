0305cfcec9c50d2bea8435b3e0c5c6becd809cdf
Carmen Alvarez <carmen@rmen.ca>
2023-07-29 17:28:37 +0200

    Make the codecheck script fail in error if any of the checks fail in error.

--- scripts/codecheck.sh
+++ scripts/codecheck.sh
@@ -1,6 +1,8 @@
+error=0
 for project in slackhealthbot alembic tests
 do
-  black $project
-  ruff check $project
-  isort --profile black $project
+  black $project || error=$?
+  ruff check $project || error=$?
+  isort --profile black $project || error=$?
 done
+exit $error
