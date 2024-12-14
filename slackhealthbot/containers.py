15e6f7d483a7221574e1a9b3185d45cbf8646195
Carmen Alvarez <carmen@rmen.ca>
2024-12-14 22:37:53 +0100

    Inject the settings to fitbitpoll

--- slackhealthbot/containers.py
+++ slackhealthbot/containers.py
@@ -12,6 +12,7 @@ class Container(containers.DeclarativeContainer):
             "slackhealthbot.domain.usecases.fitbit.usecase_process_new_activity",
             "slackhealthbot.domain.usecases.slack.usecase_post_user_logged_out",
             "slackhealthbot.remoteservices.api.slack.messageapi",
+            "slackhealthbot.tasks.fitbitpoll",
         ],
     )
 
